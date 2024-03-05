ee838302e3e30cc2787fa1bfd039740b8bdcd335
Arun Mani J <j.arunmani@proton.me>
2024-03-05 13:41:24 +0530

    tree: Create examples

--- examples/callback-sink.c
+++ examples/callback-sink.c
@@ -1,13 +1,13 @@
 #include "chunker.h"
+#include "callback-sink.h"
 
-#include "gst/gst.h"
+#include <gst/gst.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 static char *input = MARS_CHUNKER_INPUT_MIC;
-static char *output = NULL;
 static char *muxer = NULL;
 
 static GOptionEntry entries[] =
@@ -15,12 +15,18 @@ static GOptionEntry entries[] =
   { "input", 'i', G_OPTION_FLAG_NONE, G_OPTION_ARG_FILENAME, &input,
     "The input audio file to chunk like \"input.wav\"; use \"mic\" to listen from mic (default)",
     "I" },
-  { "output", 'o', G_OPTION_FLAG_NONE, G_OPTION_ARG_FILENAME, &output,
-    "The output format for chunks like \"output/%02d.wav\"", "O" },
   { "muxer", 'm', G_OPTION_FLAG_NONE, G_OPTION_ARG_STRING, &muxer,
     "The muxer to encode chunks like \"wavenc\"", "M"},
   G_OPTION_ENTRY_NULL,
 };
+
+
+static void
+on_buffer_list_cb (GPtrArray *buffers)
+{
+  printf ("Got buffers: %d\n", buffers->len);
+  fflush (stdout);
+}
 
 
 int
@@ -29,6 +35,7 @@ main (int argc, char **argv)
   g_autoptr (GError) error = NULL;
   g_autoptr (GOptionContext) context;
   g_autoptr (MarsChunker) chunker = NULL;
+  GstElement *sink;
   char key = 0;
 
   context = g_option_context_new ("Chunk the audio stream by silence");
@@ -39,24 +46,30 @@ main (int argc, char **argv)
     return EXIT_FAILURE;
   }
 
-  if (input == NULL || output == NULL || muxer == NULL) {
+  if (input == NULL || muxer == NULL) {
     g_print ("Error: Input, output and muxer must be provided\n");
     return EXIT_FAILURE;
   }
 
   gst_init (&argc, &argv);
 
+  sink = mars_callback_sink_new ();
+  mars_callback_sink_set_buffer_list_callback (MARS_CALLBACK_SINK (sink), on_buffer_list_cb);
+
   chunker = g_object_new (MARS_TYPE_CHUNKER,
                           "input", input,
-                          "output", output,
+                          "sink", sink,
                           "muxer", muxer,
                           "rate", 8000,
+                          "maximum-chunk-time", 2 * GST_SECOND,
                           NULL);
+
   mars_chunker_play (chunker);
 
   if (g_strcmp0 (input, MARS_CHUNKER_INPUT_MIC) == 0) {
     printf ("Listening from microphone; enter any key to quit: ");
     scanf ("%c", &key);
+    mars_chunker_stop (chunker);
   } else {
     printf ("Waiting for %s to be chunked…\n", input);
     fflush (stdout);
